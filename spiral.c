2886d1577b0ca0322b5fc9c96217a22b1cbaa873
Aleksandra-B <sashabritsheva@gmail.com>
2017-05-10 08:03:22 +0500

    Update spiral.c

--- spiral.c
+++ spiral.c
@@ -6,27 +6,26 @@ int arr[20][20],n,t,i,max=0,index,j;
 printf("Enter size of matrix: ");
 scanf("%d",&n);
 int count=1;
-// Number of traversals
+
 int round = ceil((double)n/2);
-// For each traversal
+
 for(i=0;i<round;i++)
 {
-// Topmost row
 for(j=i;j<=n-i-1;j++)
 {
 arr[i][j]=count++;
 }
-// Rightmost column
+// Right column
 for(j=i+1;j<=n-i-1;j++)
 {
 arr[j][n-i-1]=count++;
 }
-// Bottommost row
+// Bottom row
 for(j=n-i-2;j>=i;j--)
 {
 arr[n-i-1][j]=count++;
 }
-// Leftmost column
+// Left column
 for(j=n-i-2;j>i;j--)
 {
 arr[j][i]=count++;
