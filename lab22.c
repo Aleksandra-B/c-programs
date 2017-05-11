d437c7a6b51e2f0c4d36417741dab50abb8c3f27
Aleksandra-B <sashabritsheva@gmail.com>
2017-05-11 12:51:37 +0600

    Add files via upload

--- lab22.c
+++ lab22.c
@@ -1,26 +1,42 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <math.h>
-
 
 int main()
 {
-  int n,m;
+  int n,m,i;
   double a,b,d,af,value;
 
-  
-  
-  printf("Enter 2 integers and two float numbers:");
-  scanf("%d %d %lf %lf", &n, &m, &a, &b);
-  d=(a-b)/ (n-m); 
+   printf("Enter 2 integers and two float numbers:");
+   scanf("%d%d%lf%lf", &n, &m, &a, &b);
+
+printf("n: %d, m: %d\n", n,m);
+  if (n>m) {
+  d=(a-b)/ (n-m);
   af= a-d*n +d;
-  printf("differenece is %f and the first term is %f \n", d,af);
-  printf("arithmetic series between given numbers is: ");
-    for ( int i = n; i <= m; i++)
-  {
-  value= af+(i-1)*d;
-  
-  printf("%f ", value);
-  }
-  printf("\n");
-  return 0;
-}
+   printf("differenece is %f and the first term is %f \n", d,af);
+   printf("arithmetic series between given numbers is: ");
+   for (i = m; i <= n; i++)
+   {
+   value= af+(i-1)*d;
+   printf("%f ", value);
+   }
+   }
+   else {
+   d=(b-a)/ (m-n);
+   af= b-d*m +d;
+
+   printf("differenece is %f and the first term is %f \n", d,af);
+   printf("arithmetic series between given numbers is: ");
+
+
+   for ( i = n; i <= m; i++)
+   {
+
+   value= af+(i-1)*d;
+   printf("%f ", value);
+   }
+
+   }
+   printf("\n");
+   return 0;
+ }
