3b460d82141f8af161370672e03a3d1dd200f853
Aleksandra-B <sashabritsheva@gmail.com>
2017-05-18 11:53:54 +0500

    Add files via upload

--- lab33.c
+++ lab33.c
@@ -1,55 +1,66 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <limits.h>
+unsigned char  *ar1=NULL;
 
-void *GenArray (int n, int a, int b)
+unsigned char *GenArray (int n, int a, int b)
 {
 int i;
-int *arr=NULL;
-arr=(int*)malloc(n* sizeof(int));
+ar1=(unsigned char *)malloc(n* sizeof(unsigned char));
 for (i=0;i<n;++i){
-	arr[i]= rand()%(b+1-a)+a;		
+	ar1[i]= rand()%(b+1-a)+a;
 	}
-	return arr;
+	return ar1;
 }
+
+void SortArray (unsigned char *arr, int len)
+{
+  int i, j,q,w,e;
+  q=0;
+  w=0;
+  e=0;
+  for (i = 0; i < len; i++)
+      {
+      if (arr[i] == 1)
+	  q++;
+      if (arr[i] == 2)
+	  w++;
+      if (arr[i] == 3)
+	  e++;
+      }
+
+  for (j = 0; j < q; j++)
+       arr[j] = 1;
+  for (j = q; j < (w+q); j++)
+       arr[j] = 2;
+  for (j = (w+q); j < len; j++)
+       arr[j] = 3;
+
+  return 0;
+}
+
+
 
 
 int main()
 {
-int q,w,e;
-int  n,i,j,k,l;
-int *p;
-printf("Enter number of elements in array:");
-scanf("%i", &n);
-p=GenArray(n, 1, 3);
-printf("Your array of random numbers, which is not sorted:");
-for (i=0;i<n;i++){
-   printf("%d ",*(p+i));
-		}
-q=0;
-w=0;
-e=0;		   
-for (i=0;i<n;i++){
-	if (*(p+i)==1)
-	q++;
-	if (*(p+i)==2)
-	w++;
-	else {
-	e++;
-	}
-}		   
-	printf("\nNumber of insertions of 1,2,3: ")	  ; 
-	printf("%i %i %i\n",q,w,e)	;
-	printf("\nYour array, which is sorted:")	  ;
-	for(j=0;j<q;j++)
-	printf("%i ",1);
-	for(k=0;k<w;k++)
-	printf("%i ",2);
-	for(l=0;l<e;l++)
-	printf("%i ",3);
-	printf("\n")	;
-	
-	
-	return 0 ;
-}
+  int n, i,k;
+  k=0;
 
+  printf("Enter length of array : ");
+  scanf("%d", &n);
+
+  ar1 = GenArray (n, 1, 3);
+  for (i = 0; i < n; i++)
+      printf ("%d ", ar1[i]);
+      printf ("\n");
+
+  SortArray (ar1, n);
+  for (k = 0; k < n; k++)
+      printf ("%d ", ar1[k]);
+
+
+  free(ar1);
+
+  return 0;
+}
