dd540cbaadce044a1402a0a6d6c1156679e66cde
Aleksandra-B <sashabritsheva@gmail.com>
2017-05-18 09:00:28 +0500

    Add files via upload

--- t.c
+++ t.c
@@ -5,28 +5,6 @@
 #include <string.h>
 #define _GNU_SOURCE
 
-
-int main() 
-{
-char *st1;
-int len,n,i;
-char *u;
-printf("Enter string:");
-gets(st1);
-printf("Enter integer:");
-scanf("%i", &n);
-len=strlen(st1);
-printf("length of the string is %i\n",len);
-u =func(st1, n);
-while  (*u != NULL){
-   printf("%c\n",*(u+i));
-      i++;
-		}
-
-printf("%s", *u);
-  
-  return 0 ;
-}
 void *func(char *str,int num)
 {
 int l,i;//given string
@@ -42,5 +20,22 @@ l= strlen(str);//length of string
  	for (i=l;i<num;i++)
  	str1[i]= '*';
  }
- return str1;
+ return(str1);
+}
+
+int main() 
+{
+char *st1;
+int len,n,i;
+char *u;
+printf("Enter string:");
+gets(st1);
+puts(st1);
+printf("Enter integer:");
+scanf("%i", &n);
+len=strlen(st1);
+printf("length of the string is %i\n",len);
+u=func(st1, n);
+puts(u);
+  return 0 ;
 }
